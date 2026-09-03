aa917c5b956cb686c4575ce260f95580cfb50b7d
ttinchoromero <martinromero2845@gmail.com>
2026-09-03 20:39:42 -0300

    Revert "Remove pruebita.py test file"
    This reverts commit 8115e58ba4dbd46501819185dac3c9da468f9e4f.

---
+++ pruebita.py
@@ -0,0 +1,23 @@
+print("Hola Mundo")
+
+a = 1
+
+b= 4
+
+c= a * b
+
+
+print(c)
+
+
+
+print("Goddamnnnnn")
+
+
+print("Esto a una prueba de la rama")
+
+
+print("se puede?")
+
+
+print("Skinny nigga")

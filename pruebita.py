2aca70cb2d928e12c6d27cb2fbb0b29a5a58aa38
ttinchoromero <martinromero2845@gmail.com>
2026-09-03 20:44:01 -0300

    Update pruebita.py

--- pruebita.py
+++ pruebita.py
@@ -1,23 +1,2 @@
 print("Hola Mundo")
-
-a = 1
-
-b= 4
-
-c= a * b
-
-
-print(c)
-
-
-
-print("Goddamnnnnn")
-
-
-print("Esto a una prueba de la rama")
-
-
-print("se puede?")
-
-
-print("Skinny nigga")
+print("qodawacho")
